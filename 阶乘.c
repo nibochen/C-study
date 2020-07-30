f761cf3d6fbd8bc9d2b56106c80325a76b28e444
nibochen <nibochen@nbc-MAC.local>
2020-07-30 19:30:11 +0800

    乘积

--- 阶乘.c
+++ 阶乘.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 int main(){
   int n;
+  printf("输入一个数求它的阶乘：");
   scanf("%d",&n);
+  int n1=n;
   int x=1;
   int i=1;
- 
   while (x<=n)
   {
     i=i*x;
     x=x+1;
   }
-  printf("fac=%d\n",i);
+  printf("%d的阶乘=%d\n",n1,i);
 }
